0c67764520d779ad5b72e0ef63abe9488b635886
Joel <jlundie@gmail.com>
2026-04-27 21:39:05 -0400

    style: add index.js to typography components

---
+++ src/components/ui/typography/index.js
@@ -0,0 +1,4 @@
+export { default as Title } from './Title';
+export { default as Subheading } from './Subheading';
+export { default as Text } from './Text';
+export { default as Caption } from './Caption';

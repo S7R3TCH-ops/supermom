9cccd2c39dabc2112b69a3fbd5418c6df8a04bba
Joel <jlundie@gmail.com>
2026-04-27 21:44:29 -0400

    refactor(typography): modernize Caption component with named export and component prop

--- src/components/ui/typography/Caption.jsx
+++ src/components/ui/typography/Caption.jsx
@@ -1,16 +1,19 @@
 import { useAppTheme } from '../../../context/AppThemeContext';
 
-export default function Caption({ children, style, ...props }) {
+export const Caption = ({ children, component: Component = 'div', style, ...props }) => {
   const { T } = useAppTheme();
   return (
-    <div style={{
+    <Component style={{
       fontFamily: T.font,
       color: T.inkMuted,
       fontSize: 11,
       fontWeight: 500,
+      margin: 0,
       ...style
     }} {...props}>
       {children}
-    </div>
+    </Component>
   );
-}
+};
+
+export default Caption;
